08dbba70e08693dd4087c910a32385a3157f7835
KlimchukKS <112842539+KlimchukKS@users.noreply.github.com>
2022-11-11 19:28:48 +0500

    Update main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include "paginator.h"
 #include "read_input_functions.h"
 #include "string_processing.h"
@@ -17,19 +16,16 @@ int main() {
     search_server.AddDocument(3, std::string("big cat fancy collar "), DocumentStatus::ACTUAL, {1, 2, 8});
     search_server.AddDocument(4, std::string("big dog sparrow Eugene"), DocumentStatus::ACTUAL, {1, 3, 2});
     search_server.AddDocument(5, std::string("big dog sparrow Vasiliy"), DocumentStatus::ACTUAL, {1, 1, 1});
-    // 1439 запросов с нулевым результатом
     for (int i = 0; i < 1439; ++i) {
         request_queue.AddFindRequest(std::string("empty request"));
     }
     std::cout << std::string("empty requests: ") << request_queue.GetNoResultRequests() << std::endl;
-    // все еще 1439 запросов с нулевым результатом
     request_queue.AddFindRequest(std::string("curly dog"));
     std::cout << std::string("empty requests: ") << request_queue.GetNoResultRequests() << std::endl;
-    // новые сутки, первый запрос удален, 1438 запросов с нулевым результатом
     request_queue.AddFindRequest(std::string("big collar"));
     std::cout << std::string("empty requests: ") << request_queue.GetNoResultRequests() << std::endl;
-    // первый запрос удален, 1437 запросов с нулевым результатом
     request_queue.AddFindRequest(std::string("sparrow"));
     std::cout << std::string("Total empty requests: ") << request_queue.GetNoResultRequests() << std::endl;
+    
     return 0;
 } 
