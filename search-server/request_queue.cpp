abfd3c5adb4544bed4565d7e631676a6c0e933af
KlimchukKS <112842539+KlimchukKS@users.noreply.github.com>
2022-11-11 18:05:37 +0500

    Update request_queue.cpp

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -1,8 +1,19 @@
 #include "request_queue.h"
 
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus status) {
+    return AddFindRequest(raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
+                return document_status == status;
+                });
+}
+
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query) {
+    return AddFindRequest(raw_query, DocumentStatus::ACTUAL);
+}
+
 RequestQueue::RequestQueue(const SearchServer& search_server)
     : search_server_(search_server) {
 }
+
 int RequestQueue::GetNoResultRequests() const {
     return no_result_requests;
 }
