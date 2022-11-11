156a757962bcb6d4ef42e58f101eb984ec4a8cdf
KlimchukKS <112842539+KlimchukKS@users.noreply.github.com>
2022-11-11 17:56:50 +0500

    Update request_queue.cpp

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -1,7 +1,7 @@
 #include "request_queue.h"
 
 RequestQueue::RequestQueue(const SearchServer& search_server)
-    : search_server_(search_server){
+    : search_server_(search_server) {
 }
 int RequestQueue::GetNoResultRequests() const {
     return no_result_requests;
