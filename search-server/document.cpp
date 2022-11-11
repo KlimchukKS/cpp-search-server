7c3c84c48b242e15981ea23a645f35268568cd1c
KlimchukKS <112842539+KlimchukKS@users.noreply.github.com>
2022-11-11 17:52:41 +0500

    Update document.cpp

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -1,7 +1,14 @@
 #include "document.h"
+#include <ostream>
 
 Document::Document(int id, double relevance, int rating)
     : id(id)
     , relevance(relevance)
     , rating(rating) {
+}
+
+std::ostream& operator<<(std::ostream& output,  const Document& document) { 
+    output << std::string("{ document_id = ") << document.id << std::string(", relevance = ")
+           << document.relevance << std::string(", rating = ") << document.rating << std::string(" }");
+    return output;
 }
