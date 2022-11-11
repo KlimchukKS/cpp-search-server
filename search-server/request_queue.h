8c764bc908cac212c06d885b005c7557804b43ca
KlimchukKS <112842539+KlimchukKS@users.noreply.github.com>
2022-11-11 18:06:29 +0500

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -8,31 +8,33 @@
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
+    
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        std::vector<Document> documents = search_server_.FindTopDocuments(raw_query, document_predicate);
-        if(documents.empty() && (total_requests <= min_in_day_)){
-            ++no_result_requests;
-        }else{
-            if(no_result_requests > 0 && total_requests >= min_in_day_){
-                --no_result_requests;
-            }
-        }
-        ++total_requests;
-        return documents;
-    }
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status) {
-        return AddFindRequest(raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
-                return document_status == status;
-            });
-    }
-    std::vector<Document> AddFindRequest(const std::string& raw_query) {
-        return AddFindRequest(raw_query, DocumentStatus::ACTUAL);
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
+    
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+    
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+    
     int GetNoResultRequests() const;
+    
 private:
     int total_requests = 0;
     int no_result_requests = 0;
     const static int min_in_day_ = 1440;
     const SearchServer& search_server_;
 }; 
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    std::vector<Document> documents = search_server_.FindTopDocuments(raw_query, document_predicate);
+    if(documents.empty() && (total_requests <= min_in_day_)) {
+        ++no_result_requests;
+    } else {
+        if(no_result_requests > 0 && total_requests >= min_in_day_) {
+            --no_result_requests;
+        }
+    }
+    ++total_requests;
+    return documents;
+}
