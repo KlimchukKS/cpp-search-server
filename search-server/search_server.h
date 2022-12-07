21a7789b9ace638e0965938b19b68a2d75b73b31
KlimchukKS <112842539+KlimchukKS@users.noreply.github.com>
2022-12-07 20:29:53 +0500

    Update search_server.h

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -17,7 +17,7 @@ class SearchServer {
 public:
     template <typename StringContainer>
     explicit SearchServer(const StringContainer& stop_words);
-    
+
     explicit SearchServer(const std::string& stop_words_text);
 
     void AddDocument(int document_id, const std::string& document, DocumentStatus status,
@@ -25,17 +25,23 @@ public:
 
     template <typename DocumentPredicate>
     std::vector<Document> FindTopDocuments(const std::string& raw_query, DocumentPredicate document_predicate) const;
-    
+
     std::vector<Document> FindTopDocuments(const std::string& raw_query, DocumentStatus status) const;
-    
+
     std::vector<Document> FindTopDocuments(const std::string& raw_query) const;
-    
+
     int GetDocumentCount() const;
-    
-    int GetDocumentId(int index) const;
-    
+
+    std::vector<int>::iterator begin();
+
+    std::vector<int>::iterator end();
+
     std::tuple<std::vector<std::string>, DocumentStatus> MatchDocument(const std::string& raw_query, int document_id) const;
-    
+
+    const std::map<std::string, double>& GetWordFrequencies(int document_id) const;
+
+    void RemoveDocument(int document_id);
+
 private:
     struct DocumentData {
         int rating;
@@ -43,15 +49,16 @@ private:
     };
     const std::set<std::string> stop_words_;
     std::map<std::string, std::map<int, double>> word_to_document_freqs_;
+    std::map<int, std::map<std::string, double>> word_frequency_by_document_id_;
     std::map<int, DocumentData> documents_;
-    std::vector<int> document_ids_;
+    std::vector<int> documents_id_;
 
     bool IsStopWord(const std::string& word) const;
 
     static bool IsValidWord(const std::string& word);
-    
+
     std::vector<std::string> SplitIntoWordsNoStop(const std::string& text) const;
-    
+
     static int ComputeAverageRating(const std::vector<int>& ratings);
 
     struct QueryWord {
@@ -68,33 +75,33 @@ private:
     };
 
     Query ParseQuery(const std::string& text) const;
-    
+
     double ComputeWordInverseDocumentFreq(const std::string& word) const;
-    
+
     template <typename DocumentPredicate>
     std::vector<Document> FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const;
 };
 
 template <typename StringContainer>
 SearchServer::SearchServer(const StringContainer& stop_words)
-    : stop_words_(MakeUniqueNonEmptyStrings(stop_words)) {
+        : stop_words_(MakeUniqueNonEmptyStrings(stop_words)) {
     if (!std::all_of(stop_words_.begin(), stop_words_.end(), IsValidWord)) {
         throw std::invalid_argument(std::string("Some of stop words are invalid"));
     }
 }
 
 template <typename DocumentPredicate>
-std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_query, DocumentPredicate document_predicate) const { 
+std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_query, DocumentPredicate document_predicate) const {
     const Query query = ParseQuery(raw_query);
     auto matched_documents = FindAllDocuments(query, document_predicate);
-    
+
     std::sort(matched_documents.begin(), matched_documents.end(),
-        [](const Document& lhs, const Document& rhs) {
-            if (std::abs(lhs.relevance - rhs.relevance) < kEpsilon) {
-                return lhs.rating > rhs.rating;
-            }
-            return lhs.relevance > rhs.relevance;
-        });
+              [](const Document& lhs, const Document& rhs) {
+                  if (std::abs(lhs.relevance - rhs.relevance) < kEpsilon) {
+                      return lhs.rating > rhs.rating;
+                  }
+                  return lhs.relevance > rhs.relevance;
+              });
     if (matched_documents.size() > kMaxResultDocumentCount) {
         matched_documents.resize(kMaxResultDocumentCount);
     }
@@ -112,7 +119,7 @@ std::vector<Document> SearchServer::FindAllDocuments(const Query& query, Documen
         for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
             const auto& document_data = documents_.at(document_id);
             if (document_predicate(document_id, document_data.status, document_data.rating)) {
-                 document_to_relevance[document_id] += term_freq * inverse_document_freq;
+                document_to_relevance[document_id] += term_freq * inverse_document_freq;
             }
         }
     }
