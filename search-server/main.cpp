f33f9fd19e2ed22894aa6b30cfc52454e5654436
KlimchukKS <klinchuk1996@yandex.ru>
2023-02-24 20:54:33 +0500

    Update main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "paginator.h"
 #include "read_input_functions.h"
 #include "string_processing.h"
@@ -7,34 +6,76 @@
 #include "document.h"
 #include "log_duration.h"
 #include "remove_duplicates.h"
-#include "test_example_functions.h"
+#include "test_search_server.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <execution>
+#include <random>
 
 using namespace std;
 
-int main() {
-    
-    TestSearchServer();
-    
-    {
-        LOG_DURATION_STREAM("Time work program", std::cerr);
-        SearchServer search_server(std::string("and in at"));
-        RequestQueue request_queue(search_server);
-        search_server.AddDocument(1, std::string("curly cat curly tail"), DocumentStatus::ACTUAL, {7, 2, 7});
-        search_server.AddDocument(2, std::string("curly dog and fancy costd::llar"), DocumentStatus::ACTUAL, {1, 2, 3});
-        search_server.AddDocument(3, std::string("big cat fancy collar "), DocumentStatus::ACTUAL, {1, 2, 8});
-        search_server.AddDocument(4, std::string("big dog sparrow Eugene"), DocumentStatus::ACTUAL, {1, 3, 2});
-        search_server.AddDocument(5, std::string("big dog sparrow Vasiliy"), DocumentStatus::ACTUAL, {1, 1, 1});
-        for (int i = 0; i < 1439; ++i) {
-            request_queue.AddFindRequest(std::string("empty request"));
-        }
-        std::cout << std::string("empty requests: ") << request_queue.GetNoResultRequests() << std::endl;
-        request_queue.AddFindRequest(std::string("curly dog"));
-        std::cout << std::string("empty requests: ") << request_queue.GetNoResultRequests() << std::endl;
-        request_queue.AddFindRequest(std::string("big collar"));
-        std::cout << std::string("empty requests: ") << request_queue.GetNoResultRequests() << std::endl;
-        request_queue.AddFindRequest(std::string("sparrow"));
-        std::cout << std::string("Total empty requests: ") << request_queue.GetNoResultRequests() << std::endl;
+string GenerateWord(mt19937& generator, int max_length) {
+    const int length = uniform_int_distribution(1, max_length)(generator);
+    string word;
+    word.reserve(length);
+    for (int i = 0; i < length; ++i) {
+        word.push_back(uniform_int_distribution('a', 'z')(generator));
     }
-
-    return 0;
+    return word;
+}
+vector<string> GenerateDictionary(mt19937& generator, int word_count, int max_length) {
+    vector<string> words;
+    words.reserve(word_count);
+    for (int i = 0; i < word_count; ++i) {
+        words.push_back(GenerateWord(generator, max_length));
+    }
+    words.erase(unique(words.begin(), words.end()), words.end());
+    return words;
+}
+string GenerateQuery(mt19937& generator, const vector<string>& dictionary, int word_count, double minus_prob = 0) {
+    string query;
+    for (int i = 0; i < word_count; ++i) {
+        if (!query.empty()) {
+            query.push_back(' ');
+        }
+        if (uniform_real_distribution<>(0, 1)(generator) < minus_prob) {
+            query.push_back('-');
+        }
+        query += dictionary[uniform_int_distribution<int>(0, dictionary.size() - 1)(generator)];
+    }
+    return query;
+}
+vector<string> GenerateQueries(mt19937& generator, const vector<string>& dictionary, int query_count, int max_word_count) {
+    vector<string> queries;
+    queries.reserve(query_count);
+    for (int i = 0; i < query_count; ++i) {
+        queries.push_back(GenerateQuery(generator, dictionary, max_word_count));
+    }
+    return queries;
+}
+template <typename ExecutionPolicy>
+void Test(string_view mark, const SearchServer& search_server, const vector<string>& queries, ExecutionPolicy&& policy) {
+    LOG_DURATION(mark);
+    double total_relevance = 0;
+    for (const string_view query : queries) {
+        for (const auto& document : search_server.FindTopDocuments(policy, query)) {
+            total_relevance += document.relevance;
+        }
+    }
+    cout << total_relevance << endl;
+}
+#define TEST(policy) Test(#policy, search_server, queries, execution::policy)
+int main() {
+    mt19937 generator;
+    const auto dictionary = GenerateDictionary(generator, 1000, 10);
+    const auto documents = GenerateQueries(generator, dictionary, 10'000, 70);
+    SearchServer search_server(dictionary[0]);
+    for (size_t i = 0; i < documents.size(); ++i) {
+        search_server.AddDocument(i, documents[i], DocumentStatus::ACTUAL, {1, 2, 3});
+    }
+    const auto queries = GenerateQueries(generator, dictionary, 100, 70);
+    TEST(seq);
+    TEST(par);
 }
