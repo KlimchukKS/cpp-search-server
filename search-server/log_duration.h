33a1aad312206dcb3f9202391b67b352ae05f4f0
KlimchukKS <klinchuk1996@yandex.ru>
2023-02-24 20:55:35 +0500

    Update log_duration.h

--- search-server/log_duration.h
+++ search-server/log_duration.h
@@ -2,6 +2,7 @@
 
 #include <chrono>
 #include <iostream>
+#include <string_view>
 
 #define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
 #define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
@@ -11,11 +12,11 @@
 
 class LogDuration {
 public:
-
     using Clock = std::chrono::steady_clock;
 
-    LogDuration(const std::string& id, std::ostream& os = std::cerr)
-            : id_(id), os_(os) {
+    LogDuration(std::string_view id, std::ostream& dst_stream = std::cerr)
+            : id_(id)
+            , dst_stream_(dst_stream) {
     }
 
     ~LogDuration() {
@@ -24,11 +25,11 @@ public:
 
         const auto end_time = Clock::now();
         const auto dur = end_time - start_time_;
-        os_ << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+        dst_stream_ << id_ << ": "sv << duration_cast<milliseconds>(dur).count() << " ms"sv << std::endl;
     }
 
 private:
     const std::string id_;
-    std::ostream& os_;
     const Clock::time_point start_time_ = Clock::now();
+    std::ostream& dst_stream_;
 };
