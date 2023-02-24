5e1519ce821c853becf9592b4fcd13ea955282e0
KlimchukKS <klinchuk1996@yandex.ru>
2023-02-24 21:02:29 +0500

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -1,20 +1,14 @@
 #include "string_processing.h"
 
-std::vector<std::string> SplitIntoWords(const std::string& text) {
-    std::vector<std::string> words;
-    std::string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        } else {
-            word += c;
-        }
+std::vector<std::string_view> SplitIntoWords(std::string_view str) {
+    std::vector<std::string_view> result;
+    str.remove_prefix(std::min(str.find_first_not_of(" "), str.size()));
+
+    while (str.size() != str.npos && !str.empty()) {
+        result.push_back(str.substr(0u, str.find(' ')));
+        str.remove_prefix(std::min(str.find(" "), str.size()));
+        str.remove_prefix(std::min(str.find_first_not_of(" "), str.size()));
     }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-    return words;
+
+    return result;
 }
